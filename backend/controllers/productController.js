0b0f945c79d73cc738b59d77f34954c32b8f3941
CHINMAY <ccgambhirrao@gmail.com>
2025-07-20 21:43:38 +0530

    Added the api and successfully connected to the mongoDB

---
+++ backend/controllers/productController.js
@@ -0,0 +1,58 @@
+const Product = require("../models/productModel");
+
+// for CREATE
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// for READ ALL
+const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+// for READ ONE
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// for UPDATE
+const updateProduct = async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updated);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// for DELETE
+const deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
